d02a3d07ce29e61a08f64f1a90cefd90ea13a98f
Will <william.yizima.mazzatech@infracommerce.com.br>
2021-05-28 09:33:43 -0300

    insert comments in app/main.py

--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 
 
 ## Machine Learning
+- [Repositório com notebook](https://github.com/WilliamYizima/ml-examples/tree/master/01-Linear-Regression)
 
 ## API
 
+- [Repositório com API](https://github.com/WilliamYizima/ml-examples/tree/master/app)
+- dentro da pasta app:
+Instalar o que é necessário para rodar a api
+```bash
+pip install -r requirements.txt
+```
+- para rodar dentro da pasta app:
+```bash
+python main.py
+```
--- app/main.py
+++ app/main.py
@@ -3,27 +3,36 @@ import pickle
 import numpy as np
 
 PORT =  80
-model_name = 'teste'
 app = Flask(__name__)
 
 
 @app.route('/analyze', methods=["POST"])
 def analyze():
+    """Predict a price house
+        [payload]: {
+	                "sqft_lot": int, -> param x for the model
+	                "model_name":str -> in path "models" , name of binary
+                    }
+    """
     try:
+        
         payload = request.json
         sqft_lot = payload['sqft_lot']
-        
         model_name = payload['model_name']
         
+        # transform sqft_lot 
         x_feature = np.array([7242]).reshape(1,1)
         
+        # read model (binary file)
         modelo_carregado = pickle.load(
                                     open(
                                         f'./models/{model_name}.sav','rb'
                                         ))
+        
+        # use predict method(linear regression)
         predict = modelo_carregado.predict(x_feature)[0]
 
-        return jsonify({'price_estimated':str(predict),'sqft_lot': sqft_lot}), 200
+        return jsonify({'price_predict':str(predict),'sqft_lot': sqft_lot}), 200
     except Exception as e:
         return jsonify({'message':e}), 400
 
